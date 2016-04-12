780d8027b404fd33bb7bc05424a9a6fe75385e7c
danielrussellLA <danielrussell16@gmail.com>
2016-04-12 15:04:59 -0700

    [feature] google map renders to page

--- Components/Body.js
+++ Components/Body.js
@@ -1,13 +1,25 @@
-import GoogleMap from 'google-map-react';
 import React from 'react';
+import GoogleMap from 'google-map-react';
 
-class Body extends React.Component{
+class Body extends React.Component {
+
+  //static defaultProps = {
+  //   center: {lat: 59.938043, lng: 30.337157},
+  //   zoom: 9
+  // }
+
   render(){
     return (
-      <div>
-      Hello World
-      </div>
-    )
+        <GoogleMap
+        bootstrapURLKeys={{key: 'AIzaSyBFiuLTGZSwhBzGqkSdyiQIyqnu1tjb6Ew'}}
+        center={[37.773972, -122.431297]}
+        zoom={12}
+        style={{width: '100%', height: 400}}
+        >
+        <div lat={59.955413} lng={30.337844}>MyPlace</div>
+        </GoogleMap>
+
+    );
   }
 }
 

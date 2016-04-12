33b94fe411f5ce0db3bf2c42059c6eb951386b9c
Jonathan Tamsut <jtamsut1993@gmail.com>
2016-04-12 12:19:04 -0700

    [feature] create nav-bar

---
+++ Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title">SF Movie Map</h1>
+        <h2 classname="about">About</h2>
+      </div>
+    )
+  }
+};
+
+export default Navbar;

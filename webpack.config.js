d55d741367ae3d7dd7063ebdc8add929998ba9ab
Jonathan Tamsut <jtamsut1993@gmail.com>
2016-04-12 10:07:57 -0700

    webpack configured

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: "./Main.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};

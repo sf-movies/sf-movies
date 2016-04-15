d31c10fa24af9bddf9d92c9e89b179f8d3eaf957
Jonathan Tamsut <jtamsut1993@gmail.com>
2016-04-15 13:37:56 -0700

    serve up static files

--- server/server.js
+++ server/server.js
@@ -6,10 +6,7 @@ const PORT = 3000;
 
 app.use(logger());
 
-app.get("/", function(req, res) {
-  res.sendFile(path.resolve('./','index.html'));
-  console.log("Serving up index.html");
-})
+app.use(express.static('static'));
 
 var server = app.listen(PORT, function() {
   console.log('Server is running at http://localhost' + server.address().port)
